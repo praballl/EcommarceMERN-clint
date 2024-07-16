095de9d9788ea67eebc8df57ddeb8ad9c8524bc1
Praballl <116371043+praballl@users.noreply.github.com>
2024-07-16 18:21:34 +0530

    added some

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-600">
-      hello world
-    </h1>
+    <div >
+      <Home></Home>
+
+    </div>
   )
 }
